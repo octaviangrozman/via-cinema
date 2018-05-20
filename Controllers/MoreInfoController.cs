9a0c867871ec0c2423f67cb3590f610ea5925e95
Nikolina <starfear@abv.bg>
2018-05-20 10:44:03 +0200

    MoreInfo page done

---
+++ Controllers/MoreInfoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using viacinema.Data;
+using viacinema.ViewModels;
+
+namespace viacinema.Controllers
+{
+    [Route("MoreInfo")]
+    public class MoreInfoController : Controller
+    {
+        public DataContext context;
+
+        public MoreInfoController(DataContext _context)
+        {
+            this.context = _context;
+        }
+
+        [Route("{id}")]
+        public IActionResult MoreInfo(int id)
+        {
+            int id_ = int.Parse(this.RouteData.Values["id"].ToString());
+            var movie = context.Movies
+                .Single(m => m.Id == id);
+            var screenings = context.Screenings
+                .Where(s => s.MovieId == movie.Id)
+                .OrderByDescending(s => s.StartTime)
+                .ToList();
+
+            return View(new MoreInfoViewModel(movie, screenings));
+        }
+    }
+}
---
+++ ViewModels/MoreInfoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using viacinema.Models;
+
+namespace viacinema.ViewModels
+{
+    public class MoreInfoViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Screening> Screenings { get; set; }
+
+        public MoreInfoViewModel(Movie movie, List<Screening> scr)
+        {
+            this.Movie = movie;
+            this.Screenings = scr;
+        }
+    }
+}
